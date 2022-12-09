0f8aead1a0821d0f7e928843599fc6a88c3d39c0
Dan Gowans <d.gowans@cityssm.on.ca>
2022-12-09 13:15:23 -0500

    add coverage testing

---
+++ configTest.js
@@ -0,0 +1,8 @@
+export const config = {
+    ips: [
+        "127.0.0.1"
+    ],
+    communityString: "public",
+    oids: ["1.3.6.1.2.1.1.5.0"]
+};
+export default config;
---
+++ configTest.ts
@@ -0,0 +1,11 @@
+import type { Config } from "./types";
+
+export const config: Config = {
+    ips: [
+        "127.0.0.1"
+    ],
+    communityString: "public",
+    oids: ["1.3.6.1.2.1.1.5.0"]
+};
+
+export default config;
