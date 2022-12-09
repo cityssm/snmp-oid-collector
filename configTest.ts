a06814d39142c146d00cb028b3f3145db92f1fdc
Dan Gowans <d.gowans@cityssm.on.ca>
2022-12-09 13:18:04 -0500

    updated test oids

--- configTest.ts
+++ configTest.ts
@@ -1,11 +1,9 @@
 import type { Config } from "./types";
 
 export const config: Config = {
-    ips: [
-        "127.0.0.1"
-    ],
+    ips: ["127.0.0.1"],
     communityString: "public",
-    oids: ["1.3.6.1.2.1.1.5.0"]
+    oids: ["1.3.6.1.2.1.1.7.0", "1.3.6.1.2.1.25.1.3.0"]
 };
 
 export default config;
