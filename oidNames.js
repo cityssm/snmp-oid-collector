3d4bfd3698085ecd11661949b841db1cec4ee886
Dan Gowans <dangowans@users.noreply.github.com>
2023-10-04 14:01:22 -0400

    add serial number name

--- oidNames.js
+++ oidNames.js
@@ -1,4 +1,5 @@
 const oids = new Map();
+oids.set("1.3.6.1.4.1.1602.1.2.1.4.0", "Canon SN");
 oids.set("1.3.6.1.4.1.1602.1.11.1.3.1.4.101", "101 : Total 1");
 oids.set("1.3.6.1.4.1.1602.1.11.1.3.1.4.108", "108 : Total (Black & White 1)");
 export function getOidName(oid) {
